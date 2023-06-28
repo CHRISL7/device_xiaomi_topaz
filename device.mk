23d080b4e4ca23a99c3dbbabbff4c866c4e2ad4a
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-28 11:57:23 -0400

    topaz: inherit dalvik vm configs
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@ PRODUCT_SOONG_NAMESPACES += \
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
