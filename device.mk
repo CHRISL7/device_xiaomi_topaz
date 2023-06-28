36b998e1b09830e3d2534fe0cde1a3306824d72c
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-28 10:34:56 -0400

    topaz: init: Inherit developer_gsi_keys
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -3,6 +3,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
