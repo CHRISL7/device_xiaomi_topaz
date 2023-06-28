ef6e8150ab8e8f9374174723692c7bf8692b8ec8
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-28 10:43:23 -0400

    topaz: Enable DEXPREOPT_GENERATE_APEX
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,10 @@
 
 DEVICE_PATH := device/xiaomi/topaz
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
 
+# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
