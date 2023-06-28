01e508eaf7992fb68e9b11c70a465500159dfd02
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-28 15:03:17 -0400

    topaz: Build AIDL drm service
    [1] - topaz use this
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -175,7 +175,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey \
+    android.hardware.drm-service.widevine
 
 PRODUCT_VENDOR_PROPERTIES += \
     drm.service.enabled=true
