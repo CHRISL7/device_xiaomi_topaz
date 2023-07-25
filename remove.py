509f2cfad4930068ac8028a57d8c1720cef9094f
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-07-25 17:12:46 +0000

    mm
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- remove.py
+++ remove.py
@@ -4,8 +4,6 @@ import os
 import sys
 import getopt
 
-MY_DIR = os.path.dirname(os.path.abspath(__file__))
-ANDROID_ROOT = os.path.abspath(os.path.join(MY_DIR, '..', '..', '..'))
 QCOM_COMMON_PATH = 'device/qcom/common'
 TARGET_FILE = 'proprietary-files.txt'
 
@@ -27,41 +25,50 @@ def remove_lines_from_file(lines, file_path):
     return removed_lines
 
 def main(argv):
-    component = None
+    components = []
+    dry_run = False
+
     try:
-        opts, args = getopt.getopt(argv, "c:")
+        opts, args = getopt.getopt(argv, "c:d")
     except getopt.GetoptError:
-        print("Invalid option: Use -c to specify the component.")
+        print("Invalid option: Use -c to specify the component(s) and -d for a dry run.")
         sys.exit(1)
 
     for opt, arg in opts:
         if opt == '-c':
-            component = arg
+            components.append(arg)
+        elif opt == '-d':
+            dry_run = True
 
-    if not component:
-        print("Component not specified. Use -c to specify the component.")
+    if not components:
+        print("Component(s) not specified. Use -c to specify the component(s).")
         sys.exit(1)
+
+    current_dir = os.getcwd()
+    target_file_path = os.path.join(current_dir, TARGET_FILE)
 
     removed_lines = []
     for division in ('system', 'vendor'):
-        component_file_path = os.path.join(ANDROID_ROOT, QCOM_COMMON_PATH, division, component, TARGET_FILE)
-        if not os.path.isfile(component_file_path):
-            continue
+        for component in components:
+            component_file_path = os.path.join(current_dir, QCOM_COMMON_PATH, division, component, TARGET_FILE)
+            if not os.path.isfile(component_file_path):
+                continue
 
-        with open(TARGET_FILE, 'r') as target_file:
-            lines = target_file.readlines()
+            with open(target_file_path, 'r') as target_file:
+                lines = target_file.readlines()
 
-        removed_lines += remove_lines_from_file(lines, component_file_path)
+            removed_lines += remove_lines_from_file(lines, component_file_path)
 
-        with open(TARGET_FILE, 'w') as target_file:
-            target_file.write(''.join(lines))
+            if not dry_run:
+                with open(target_file_path, 'w') as target_file:
+                    target_file.write(''.join(lines))
 
     if removed_lines:
-        print(f"The following lines were removed from the file '{TARGET_FILE}':")
+        print(f"The following lines {'would be removed from' if dry_run else 'were removed from'} the file '{target_file_path}':")
         for line in removed_lines:
             print(f"\033[31m- {line}\033[0m")
     else:
-        print(f"No lines were removed from the file '{TARGET_FILE}'.")
+        print(f"No lines were removed from the file '{target_file_path}'.")
 
 if __name__ == '__main__':
     main(sys.argv[1:])
