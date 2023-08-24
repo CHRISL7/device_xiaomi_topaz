66b2fe5bcea4f41f22fe0a80f43a8f25507de8bf
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-08-24 23:06:33 +0000

    topaz: init: Add xiaomi perf changes
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -353,6 +353,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.target.rc \
     init.topaz.rc \
+    init.topaz.perf.rc \
     init.xiaomi.fingerprint.rc \
     init.xiaomi.rc \
     ueventd.xiaomi.rc
