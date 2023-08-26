15386b6280b6a2afa3ce96832942070c789a4e6e
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-08-26 01:17:47 +0000

    topaz: Import releasetools definition from xiaomi sm6225-common
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,6 +142,9 @@ SOONG_CONFIG_NAMESPACES += ufsbsg
 SOONG_CONFIG_ufsbsg += ufsframework
 SOONG_CONFIG_ufsbsg_ufsframework := bsg
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 # Security patch level
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
