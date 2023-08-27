378792f26ec3db0cc97c38d9cacb192083411caa
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-08-27 15:57:37 +0000

    Revert "topaz: Permissive build for now"
    This reverts commit 1d6a63bf1aa9e2474fbb6640876c5416428d5e04.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,8 +81,6 @@ BOARD_BOOTCONFIG := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=4e00000.dwc3
 
-BOARD_BOOTCONFIG += androidboot.selinux=permissive
-
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := topaz,tapas
 
